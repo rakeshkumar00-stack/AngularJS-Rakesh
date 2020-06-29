41d63ccf99fff8ac48183162f23a3078160206e1
RAKESH <rakeshkumarsadotra@gmail.com>
2020-06-29 21:47:38 +0530

    Mod3 Sol

--- Mod3-Sol/app.js
+++ Mod3-Sol/app.js
@@ -10,7 +10,7 @@
     function FoundItems() {
         var ddo = {
             restrict: 'E',
-            templateUrl: 'foundItems.html',
+            templateUrl: 'foundDisplay.html',
             scope: {
                 foundItems: '<',
                 onEmpty: '<',
